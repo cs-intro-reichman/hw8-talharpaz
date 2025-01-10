9e24bbfe0977182bb0867cf2ed73bf99c2cf29ed
talharpaz <tal.harpaz01@gmail.com>
2025-01-10 11:52:41 +0200

    try4

--- User.java
+++ User.java
@@ -62,15 +62,25 @@
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
         name=capitalize(name);
+         
         for (int i = 0; i < follows.length; i++) {
+            if ( follows[i] == name || isArrFull(follows)) return false;
+            
             if (follows[i]== null){
                 follows[i] = name;  
-                fCount++; 
+                fCount++;
+                break; 
             }
      }
     return true;
     }
-   
+    public boolean isArrFull (String [] arr){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) return false;
+            
+        }
+        return true;
+    }
 
     /** Removes the given name from the follows list of this user. If successful, returns true.
      *  If the name is not in the list, does nothing and returns false. */
